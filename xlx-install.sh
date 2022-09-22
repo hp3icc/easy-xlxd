b9e92bf8a18049859c6264da07272aecd8634bf3
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-09-22 14:49:23 -0500

    Update xlx-install.sh

--- xlx-install.sh
+++ xlx-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo rm -r /root/reflector-install-files/xlxd
+sudo rm -r /root/xlxd
 # A tool to install xlxd, your own D-Star Reflector.
 # For more information, please visit: https://n5amd.com
 #Lets begin-------------------------------------------------------------------------------------------------
@@ -23,7 +24,8 @@ XLXDREPO=https://github.com/n7tae/new-xlxd.git
 #https://github.com/LX3JL/xlxd.git
 DMRIDURL=http://xlxapi.rlx.lu/api/exportdmr.php
 WEBDIR=/var/www/xlxd
-XLXINSTDIR=/root/reflector-install-files/xlxd
+#XLXINSTDIR=/root/reflector-install-files/xlxd
+XLXINSTDIR=/root/xlxd
 DEP="git build-essential apache2 php libapache2-mod-php php7.0-mbstring"
 DEP2="git build-essential apache2 php libapache2-mod-php php7.3-mbstring"
 DEP3="git build-essential apache2 php libapache2-mod-php php7.4-mbstring"
