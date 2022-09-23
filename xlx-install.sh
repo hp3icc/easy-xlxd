398ab905e5dd3dafb83591c71a4af7f5ede97470
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2022-09-23 15:43:56 -0500

    Update xlx-install.sh

--- xlx-install.sh
+++ xlx-install.sh
@@ -106,7 +106,8 @@ sed -i "s/VirtualHost \*/VirtualHost $XLXDOMAIN/g" /etc/apache2/sites-available/
 sed -i "s/html/xlxd/g" /etc/apache2/sites-available/$XLXDOMAIN.conf
 chown -R www-data:www-data /var/www/xlxd/
 chown -R www-data:www-data /xlxd/
-chmod +777 /xlxd/
+sudo chmod +777 /xlxd/
+sudo chmod +r /var/log/messages
 a2ensite $XLXDOMAIN
 service xlxd stop
 service xlxd start
