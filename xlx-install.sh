4bd1b46f5e3ebf14667109546c063edd6f388d71
Esteban Mackay Q <49044505+hp3icc@users.noreply.github.com>
2023-01-26 16:34:25 -0500

    Update xlx-install.sh

--- xlx-install.sh
+++ xlx-install.sh
@@ -292,8 +292,8 @@ service xlxd start
 sudo systemctl restart apache2
 sudo systemctl enable apache2
 sudo systemctl enable xlxd
-#sudo systemctl enable rebooter-xlxd.service
-#sudo systemctl restart rebooter-xlxd.service
+sudo systemctl enable rebooter-xlxd.service
+sudo systemctl restart rebooter-xlxd.service
 #
 echo "------------------------------------------------------------------------------"
 echo ""
